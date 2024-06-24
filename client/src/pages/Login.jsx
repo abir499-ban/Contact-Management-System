15966070622057e3a209dd7dfbaa660f53ea144f
Abir Banerjee <abirbanerjee793@gmail.com>
2024-06-24 15:54:22 +0530

    Auth_done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,23 +9,24 @@ const Login = () => {
   const handlesubmit = (e) =>{
     e.preventDefault();
     axios.post('http://localhost:8000/api/user/login', {email, password})
-    .then((data) => console.log(data))
+    .then((data) => console.log(data.data.token))
     .catch((err) => console.log(err));
   }
   return (
     <>
+    <h1>LOG IN</h1>
     <form className='mt-3' onSubmit={handlesubmit}>
         <div>
           <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={(e) => setemail(e.target.value) } />
+          <input value={email} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={(e) => setemail(e.target.value) } required/>
         </div>
         <div>
           <label for="exampleInputEmail1" class="form-label mt-4">Password</label>
-          <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter password" onChange={(e) => setpassword(e.target.value) } />
+          <input value={password} type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter password" onChange={(e) => setpassword(e.target.value) } required/>
         </div>
         <button type="submit" class="btn btn-primary mt-4">Log In</button>
       </form>
-      <p className='container mt-3'>Dont have an Account?<Link to={'/register'}> Clicke here to create one!!</Link></p>
+      <p className='container mt-3'>Dont have an Account?<Link to={'/register'}> Click here to create one!!</Link></p>
     </>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -14,19 +14,20 @@ const Register = () => {
   }
   return (
     <>
+    <h1>Register</h1>
       <form className='mt-3' onSubmit={handlesubmit}>
         <div>
           <label for="exampleInputEmail1" class="form-label mt-4">Name</label>
-          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter full name" onChange={(e) => setname(e.target.value) }/>
+          <input value={name} type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter full name" onChange={(e) => setname(e.target.value) } required/>
         </div>
         <div>
           <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={(e) => setemail(e.target.value) } />
+          <input value={email} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={(e) => setemail(e.target.value) } required/>
           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div>
           <label for="exampleInputEmail1" class="form-label mt-4">Password</label>
-          <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter password" onChange={(e) => setpassword(e.target.value) } />
+          <input value={password} type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter password" onChange={(e) => setpassword(e.target.value) } required/>
         </div>
         <button type="submit" class="btn btn-primary mt-4">Register</button>
       </form>
